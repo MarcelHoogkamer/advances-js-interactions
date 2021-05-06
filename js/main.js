2d9ec8abc89f0abfbb52d70be5d2f820a83fbd24
Mark <mark.hoogkamer@gmail.com>
2021-05-06 15:19:46 +0200

    Sticky parallax header fixed and dark mode updated

--- js/main.js
+++ js/main.js
@@ -82,8 +82,8 @@ makeElement(slide);
 //PARALLAX SLIDER
 window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
