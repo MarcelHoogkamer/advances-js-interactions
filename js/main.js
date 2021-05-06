17e432edce42f9273963446710b3657342ae6988
Mark <mark.hoogkamer@gmail.com>
2021-05-06 09:01:59 +0200

    Minor changes to gallery

--- js/main.js
+++ js/main.js
@@ -1,88 +1,77 @@
-Array.from(document.querySelectorAll(".letter")).forEach(el => {
-  el.innerText = randomLetter();
-});
-
-function randomLetter(){
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-  return alphabet[Math.floor(Math.random() * alphabet.length)]
+var objImage= null;
+function init(){
+  objImage=document.getElementById("birdimg");
+  objImage2=document.getElementById("balloon");
+  objImage.style.position='relative';
+  objImage.style.left='200px';
+  objImage.style.top='200px';
 }
 
-// DARK MODE FEATURE
-let content = document.getElementsByTagName('body')[0];
-let darkMode = document.getElementById('dark-change');
-darkMode.addEventListener("click", function() {
-  darkMode.classList.toggle('active');
-  content.classList.toggle('night');
-
-})
-
-// IMAGE SLIDER
-let slide = document.querySelectorAll(".slide");
-let dots = document.querySelectorAll(".indicator");
-let before = document.querySelector(".before");
-let id = 0;
-let last = slide.length - 1;
-let after = id + 1;
-function next() {
-  if (id === slide.length - 1) {
-    id = 0;
-    last = slide.length - 1;
-    after = 1;
-  } else {
-    id++;
-    after = id + 1;
-    last = id - 1;
+function getKeyAndMove(e){
+  var key_code=e.key;
+  switch(key_code){
+    case 'ArrowLeft':
+      moveLeft();
+      break;
+    case 'ArrowUp':
+      moveUp();
+      break;
+    case 'ArrowRight':
+      moveRight();
+      break;
+    case 'ArrowDown':
+      moveDown();
+      break;
+    case 'Shift':
+      downCrash();
+      break;
+    case 'Enter':
+      makeDove();
+      break;
+    case 'Backspace':
+      stealthMode();
+      break;
+    case 'Escape':
+      balloonDrop();
+      break;
   }
-  let second = (slide[id].style.display = "block");
-  let prev = (slide[last].style.display = "none");
-  dots[id].style.background = "dimgray";
-  dots[last].style.background = "darkgray";
-  displayPrev(slide, last);
-  displayNext(slide, after);
 }
-function prev() {
-  if (id !== 0) {
-    id--;
-    after = id - 1;
-    last = id + 1;
-  } else {
-    id = slide.length - 1;
-    after = 4;
-    last = 0;
-  }
-  let next = (slide[id].style.display = "block");
-  let prev = (slide[last].style.display = "none");
-  dots[id].style.background = "dimgray";
-  dots[last].style.background = "darkgray";
-  displayPrev(slide, after);
-  displayNext(slide, last);
-}
-function displayPrev(slide, id) {
-  let a = (id === -1) ? 5 : id;
-  document.querySelector(".before")
-    .innerHTML = slide[a].innerHTML;
-}
-function displayNext(slide, id) {
-  let a = (id === slide.length) ? 0 : id;
-  document.querySelector(".after")
-    .innerHTML = slide[a].innerHTML;
-}
-function makeElement(slide) {
-  let before = document.createElement("div");
-  before.innerHTML = slide[slide.length - 1].innerHTML;
-  before.setAttribute("class", "before");
-  let after = document.createElement("div");
-  after.innerHTML = slide[1].innerHTML;
-  after.setAttribute("class", "after");
-  let parent = document.querySelector(".slider");
-  parent.append(before, after);
-}
-makeElement(slide);
 
-//IMAGE GALLERY
+function balloonDrop(){
+  objImage2.style.top=parseInt(objImage.style.top)+50 +'px';
+  objImage2.style.top=parseInt(objImage.style.top)-50 +'px';
+}
 
-//FOLLOW THE MOUSE
+function stealthMode(){
+  objImage.style.opacity= '0.1';
+}
 
-//AVOID THE MOUSE
+function makeDove(){
+  objImage.style.filter= 'invert(100%)';
+}
+function downCrash(){
+  objImage.style.top=parseInt(objImage.style.top)+1000 +'px';
+}
+function moveLeft(){
+  objImage.style.transform= "scaleX(-1)";
+  objImage.style.left=parseInt(objImage.style.left)-5 +'px';
+}
+function moveUp(){
+  objImage.style.transform= "rotate(-90deg)";
+  objImage.style.top=parseInt(objImage.style.top)-5 +'px';
+}
+function moveRight(){
+  objImage.style.transform= "scaleX(1)";
+  objImage.style.left=parseInt(objImage.style.left)+5 +'px';
+}
+function moveDown(){
+  objImage.style.transform= "rotate(90deg)";
+  objImage.style.top=parseInt(objImage.style.top)+5 +'px';
+}
+window.onload=init;
 
-//TEXT EFFECTS
+window.addEventListener("keydown", e => {
+  getKeyAndMove(e);
+  clear;
+}
+)
